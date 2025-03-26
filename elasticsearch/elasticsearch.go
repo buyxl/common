a5b755965c97d153d5ce8139be9503d86409fda8
buyxl <1016839495@qq.com>
2025-03-26 16:06:29 +0800

    Add files via upload

---
+++ elasticsearch/elasticsearch.go
@@ -0,0 +1,22 @@
+package elasticsearch
+
+import "github.com/elastic/go-elasticsearch/v7"
+
+var (
+	ES  *elasticsearch.Client
+	err error
+)
+
+type EsClient struct {
+	Host string
+}
+
+func (e *EsClient) EsInit(Host string) {
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			Host,
+		},
+		// ...
+	}
+	ES, err = elasticsearch.NewClient(cfg)
+}
